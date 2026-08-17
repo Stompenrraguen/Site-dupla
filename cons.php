e0529c5a6cfffd8d66848d68b8860b65f949cccd
Enzo Carvalho <150471316+Stompenrraguen@users.noreply.github.com>
2026-08-17 10:09:26 -0300

    Add files via upload

---
+++ cons.php
@@ -0,0 +1,14 @@
+<?php
+               $server = "localhost";
+               $user = "Mark";
+               $password = "omega01";
+               $db = "carteira";
+               $local = "https://eun.ifba.edu.br/sistemas/carteira/validar/processar.php?matricula="
+               //$local = "http://192.168.1.12/carteira/validar/processar.php?matricula="
+ 
+?>
+
+
+
+
+
