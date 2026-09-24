a7b445dd321bbb309fdc4a151ee5b44606da442a
Enzo Carvalho <150471316+Stompenrraguen@users.noreply.github.com>
2026-09-24 17:45:34 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!isset($_SESSION)) session_start();
-
-unset($_SESSION["logado"], $_SESSION["login"], $_SESSION["cpf"], $_SESSION["nome"], $_SESSION["usuario"], $_SESSION["produto_pendente"], $_SESSION["carrinho"]);
-session_destroy();
-
-header("Location: index.php");
-exit;
-?>
